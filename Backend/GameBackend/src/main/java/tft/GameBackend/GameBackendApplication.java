426f1235cd5840209ab281afa1d93b9e891e84df
Vladislav <20031911@mail.ru>
2023-04-16 14:40:58 +0300

    feat: создана начальная структура приложения

---
+++ Backend/GameBackend/src/main/java/tft/GameBackend/GameBackendApplication.java
@@ -0,0 +1,13 @@
+package tft.GameBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameBackendApplication.class, args);
+	}
+
+}
