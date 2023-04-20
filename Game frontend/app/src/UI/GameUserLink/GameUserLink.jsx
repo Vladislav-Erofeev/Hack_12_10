3f6e4891c5daeef040bb3152fb2c8e3e629c30c9
Nikita Khitrov <hitrov.qazws@gmail.com>
2023-04-20 23:34:47 +0300

    fix: удаление папкм Game frontend

--- Game frontend/app/src/UI/GameUserLink/GameUserLink.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const GameUserLink = () => {
-  return (
-    <div>GameUserLink</div>
-  )
-}
-
-export default GameUserLink
--- Game frontend/app/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import { fieldReducer } from './slices/gameField'
-import { gameStatusReducer } from './slices/gameStatus'
-export default configureStore({
-    reducer: {
-        field: fieldReducer,
-        game_status: gameStatusReducer
-    }
-})
