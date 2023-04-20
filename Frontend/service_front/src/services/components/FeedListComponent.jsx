1a71e118f2a918c9ff0549c7560f291dc80413c8
p0pl3 <m.poplavskiy04@gmail.com>
2023-04-20 20:57:58 +0300

    fix: изменены пути к фотографиям

--- Frontend/service_front/src/services/components/FeedListComponent.jsx
+++ Frontend/service_front/src/services/components/FeedListComponent.jsx
@@ -18,7 +18,7 @@ const FeedListComponent = ({feeds}) => {
             <div className="d-flex mb-3 align-items-center">
                 <Link to={`/profile/${feed.author.id}`}>
                     <div style={{width: "50px", height: "50px"}}>
-                        <img style={{width: "100%", height: "100%", borderRadius: "100%"}} src={`http://localhost:8080${feed.author.url}`} alt=""/>
+                        <img style={{width: "100%", height: "100%", borderRadius: "100%"}} src={`http://localhost:8080/image${feed.author.url}`} alt=""/>
                     </div>
                 </Link>
                 <div className="profile-info ms-4">
@@ -28,7 +28,7 @@ const FeedListComponent = ({feeds}) => {
             </div>
             <div>
                 <p>{feed.body}</p>
-                <Link to={`/feeds/${feed.id}`}><img src={`http://localhost:8080${feed.images[0].url}`} width="100%" alt=""/></Link>
+                <Link to={`/feeds/${feed.id}`}><img src={`http://localhost:8080/image${feed.images[0].url}`} width="100%" alt=""/></Link>
             </div>
         </div>
     )))
--- Frontend/service_front/src/services/pages/Register.jsx
+++ Frontend/service_front/src/services/pages/Register.jsx
@@ -2,15 +2,41 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import {Button} from "reactstrap";
 import {reg} from "../../redux/slices/security";
+import axios from "axios";
+import Cookies from "universal-cookie";
 
 const AdminLogin = () => {
+
+    const cookies = new Cookies();
+    const token = cookies.get('token')
 
     const [admReg, setAdmReg] = useState({
         email: "",
         password: ""
     })
-
+    const [file, setFile] = useState(null)
     const dispatch = useDispatch()
+
+    const register = async () => {
+        dispatch(reg(admReg, dispatch))
+        if (file) {
+            console.log(file)
+            const formData = new FormData();
+            formData.append('file', file)
+            console.log(formData)
+            axios.post('http://localhost:8080/person_image/add', formData,
+                {
+                    headers: {
+                        "access-control-allow-origin": "http://localhost:3000",
+                        "Authorization": `Bearer ${token}`,
+                    }
+                }
+            ).catch(error => {
+                console.error('There was an error!', error);
+            });
+        }
+
+    }
 
     return (
         <>
@@ -22,9 +48,12 @@ const AdminLogin = () => {
                 <input type="password" required onChange={(event) => {
                     setAdmReg({...admReg, password: event.target.value})
                 }}/>
+                <input type="file" required onChange={(event) => {
+                    setFile(event.target.files[0])
+                }}/>
                 <Button text = "Войти" onClick={(event) => {
                     event.preventDefault()
-                    dispatch(reg(admReg, dispatch))
+                    register()
                 }}/>
             </form>
         </>
