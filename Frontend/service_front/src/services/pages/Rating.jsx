e150104fc0105812c4d3b2bf1b53b5dd8d35f0a2
p0pl3 <m.poplavskiy04@gmail.com>
2023-04-22 06:11:27 +0300

    feat: добавил страницу рейтинга

---
+++ Frontend/service_front/src/services/pages/Rating.jsx
@@ -0,0 +1,60 @@
+import {get_top_ten, get_user_rating} from "../requests";
+import React, {useEffect, useState} from 'react';
+import {useSelector} from "react-redux";
+import {selectUser} from "../../redux/slices/user";
+import Person from "../components/Person";
+import {selectToken} from "../../redux/slices/security";
+
+const Rating = () => {
+
+    const token = useSelector(selectToken)
+
+    const user = useSelector(selectUser)
+
+    const [rating, setRating] = useState(0)
+
+    const [users, setUsers] = useState([])
+
+    useEffect(() => {
+        get_top_ten(token).then(res => {
+            setUsers(res)
+        })
+    }, [user])
+
+    useEffect(() => {
+        if (user)
+            get_user_rating(token, user.id).then(res => {
+                setRating(res)
+            })
+    }, [user])
+
+    return (
+        <div className="my-container">
+            <h1 className="my-3">Рейтинг</h1>
+            {users
+                ?
+                users.map((user, index) => (
+                    <div className="d-flex p-3 align-items-center"
+                         style={{background: rating === index + 1 ? '#fcc45c' : 'none'}} key={user.id}>
+                        <h2 style={{width: "50px"}}>{index + 1}</h2>
+                        <Person user={user}/>
+                    </div>
+                ))
+                :
+                <h3>Мы никого не нашли %(</h3>
+            }
+            {rating < 10
+                ? <></>
+                :
+                <>
+                    <h1 className="text-center">...</h1>
+                    <h2>{rating}</h2>
+                    <Person user={user}/>
+                </>
+            }
+
+        </div>
+    )
+}
+
+export default Rating
