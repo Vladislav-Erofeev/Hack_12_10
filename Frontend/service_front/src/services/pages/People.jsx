1695a5c57cc6de4d8c5a4dfb9b45dc096856c484
p0pl3 <m.poplavskiy04@gmail.com>
2023-04-24 00:14:00 +0300

    feat: добавлена музыка

--- Frontend/service_front/src/services/pages/People.jsx
+++ Frontend/service_front/src/services/pages/People.jsx
@@ -1,32 +1,8 @@
 import React, {useEffect, useState} from 'react'
 import UserList from "../components/UserList";
 import {get_all_users, send_friend_request} from "../requests";
-import {useBeforeUnload, useLocation} from "react-router-dom";
 
 function People() {
-
-    const [audio] = useState(new Audio("master_of_puppets.mp3"))
-    const [playing, setPlaying] = useState(true)
-
-    useEffect(() => {
-        playing ? audio.play() : audio.pause();
-    }, [playing])
-
-    const toggle = () => setPlaying(!playing);
-
-    const location = useLocation();
-    useEffect(() => {
-        console.log(location.pathname)
-        if (location.pathname !== "/people")
-            audio.pause();
-    }, [location])
-
-    useEffect(() => {
-        return () => audio.pause()
-    }, [])
-
-
-
     return (
         <div className="my-container">
             <h1 className="my-3 titel_one">Люди</h1>
