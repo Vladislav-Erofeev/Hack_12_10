72562fe9eca4bf59ff410f434ee9af48f7fc9049
Vladislav <20031911@mail.ru>
2023-04-23 13:35:12 +0300

    fix: исправлена конфигурация cors

--- Backend/GameBackend/src/main/java/tft/GameBackend/GameBackendApplication.java
+++ Backend/GameBackend/src/main/java/tft/GameBackend/GameBackendApplication.java
@@ -22,7 +22,7 @@ public class GameBackendApplication {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedMethods("GET", "POST", "PUT", "DELETE");;
+                registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST", "PUT", "DELETE");;
             }
         };
     }
